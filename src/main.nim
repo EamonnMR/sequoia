56bcb6f2317a895773adf62fe5a5ee632c2b908c
EamonnMR <eamonn.m.r@gmail.com>
2024-08-05 08:16:10 -0400

    Parsing first steps

--- src/main.nim
+++ src/main.nim
@@ -1,12 +1,17 @@
 import std/strutils
 import std/rdstdin
 
-import std/rdstdin
+proc parse(input: string): string =
+  input.multiReplace(@[
+    ("(", " ( "),
+    (")", " ) "),
+  ])
+
 var line: string
 echo "Sequoia"
 while true:
   let ok = readLineFromStdin(">>>", line)
   if not ok: break # ctrl-C or ctrl-D will cause a break
-  if line.len > 0: echo line
+  if line.len > 0: echo parse(line)
 echo "exiting"
 
