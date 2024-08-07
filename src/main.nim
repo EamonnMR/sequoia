1318c4e2255e820d220ed509b127443b8452764c
EamonnMR <eamonn.m.r@gmail.com>
2024-08-06 22:27:18 -0400

    Sets up iterator so we can do recursive descent on the tokens

--- src/main.nim
+++ src/main.nim
@@ -1,11 +1,42 @@
 import std/strutils
 import std/rdstdin
 
-proc parse(input: string): seq[string] =
+type
+  NodeType* = enum Int, String, List
+  
+  Node* = ref object
+    case node_type*: NodeType:
+    of Int:
+      i*: int
+    of String:
+      text*: string
+    of List:
+      liet: seq[Node]
+
+proc tokenIter(tokens: sink seq[string]): iterator(): string =
+  return iterator(): string =
+    for i in tokens:
+      yield i
+
+proc tokenize(input: string): seq[string] =
   input.multiReplace(@[
     ("(", " ( "),
     (")", " ) "),
   ]).split_whitespace()
+
+
+proc parseList(getNextToken: iterator(): string): seq[string] =
+  var tokens: seq[string] = @[]
+  while true:
+    if finished( getNextToken):
+      return tokens
+    else:
+      tokens.add( getNextToken() )
+
+proc parse(input: string): seq[string] =
+  let nextToken = tokenIter( tokenize( input ) )
+  return parseList( nextToken )
+
 
 var line: string
 echo "Sequoia"
