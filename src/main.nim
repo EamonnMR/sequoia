2d239a506db8753ff2ed4fea15068e90fab6c201
EamonnMR <eamonn.m.r@gmail.com>
2024-08-09 22:39:38 -0400

    Parse lists and strings, print with indentation

--- src/main.nim
+++ src/main.nim
@@ -1,5 +1,7 @@
 import std/strutils
+import std/sequtils
 import std/rdstdin
+import std/sugar
 
 type
   NodeType* = enum Int, String, List
@@ -11,12 +13,25 @@ type
     of String:
       text*: string
     of List:
-      liet: seq[Node]
+      list: seq[Node]
 
-proc tokenIter(tokens: sink seq[string]): iterator(): string =
-  return iterator(): string =
-    for i in tokens:
-      yield i
+  TokenBuffer = ref object
+    position: int
+    buffer: seq[string]
+
+proc createTokenBuffer(tokens: sink seq[string]): TokenBuffer =
+  return TokenBuffer(position: 0, buffer: tokens)
+
+proc hasNextToken(buffer: TokenBuffer): bool =
+  return true
+  result = buffer.buffer.len() < buffer.position
+
+proc lookAheadNextToken(buffer: TokenBuffer): string =
+  return buffer.buffer[buffer.position]
+
+proc getNextToken(buffer: TokenBuffer): string =
+  result = buffer.lookAheadNextToken()
+  buffer.position += 1
 
 proc tokenize(input: string): seq[string] =
   input.multiReplace(@[
@@ -24,25 +39,40 @@ proc tokenize(input: string): seq[string] =
     (")", " ) "),
   ]).split_whitespace()
 
+proc print(node: Node, indent: int): string =
+  var whitespace: string = "\n" & (" ".repeat(indent))
+  case node.node_type:
+    of Int:
+      return whitespace & $(node.i)
+    of String:
+      return whitespace & node.text
+    of List:
+      var list_text: string = node.list.map(x => print(x, indent + 1) ).join(" ")
+      return whitespace & "(" & list_text & whitespace & ")"
 
-proc parseList(getNextToken: iterator(): string): seq[string] =
-  var tokens: seq[string] = @[]
-  while true:
-    if finished( getNextToken):
-      return tokens
-    else:
-      tokens.add( getNextToken() )
+proc `$`* (node: Node): string = print(node, 0)
 
-proc parse(input: string): seq[string] =
-  let nextToken = tokenIter( tokenize( input ) )
-  return parseList( nextToken )
+proc lex(input: string): TokenBuffer =
+  return createTokenBuffer( tokenize( input ) )
 
+proc parse(tokens: TokenBuffer): Node =
+  var token: string = tokens.getNextToken()
+  if token == "(":
+    var nodes: seq[Node] = @[]
+    while tokens.hasNextToken():
+      if tokens.lookAheadNextToken() == ")":
+        break
+      nodes.add( parse( tokens ) )
+
+    return Node(node_type: NodeType.List, list: nodes)
+
+  return Node(node_type: NodeType.String, text: token )
 
 var line: string
 echo "Sequoia"
 while true:
-  let ok = readLineFromStdin(">>>", line)
+  let ok = readLineFromStdin(">>> ", line)
   if not ok: break # ctrl-C or ctrl-D will cause a break
-  if line.len > 0: echo parse(line)
+  if line.len > 0: echo parse(lex(line))
 echo "exiting"
 
